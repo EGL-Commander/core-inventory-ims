98dec1d0c9304b938a96cef901c3fd02920e5adc
EGL Commander <jaiminmehta441@gmail.com>
2026-03-14 12:51:20 +0530

    Stock Models.
    Added Some Stock Related Models in the Database, Successfully Appearing in the Admin Panel

--- core_inventory/operations/admin.py
+++ core_inventory/operations/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Receipt, ReceiptItem, Delivery, DeliveryItem, Transfer, Adjustment
 
 # Register your models here.
+
+admin.site.register(Receipt)
+admin.site.register(ReceiptItem)
+admin.site.register(Delivery)
+admin.site.register(DeliveryItem)
+admin.site.register(Transfer)
+admin.site.register(Adjustment)
--- core_inventory/operations/models.py
+++ core_inventory/operations/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from products.models import Product
+from inventory.models import Warehouse
 
 # Create your models here.
+
+class Receipt(models.Model):
+
+    supplier = models.CharField(max_length=200)
+
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('DRAFT','Draft'),
+            ('DONE','Done')
+        ],
+        default='DRAFT'
+    )
+
+    def __str__(self):
+        return f"Receipt {self.id} - {self.supplier}"
+
+class ReceiptItem(models.Model):
+
+    receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE, related_name="items")
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    quantity = models.IntegerField()
+    
+class Delivery(models.Model):
+
+    customer = models.CharField(max_length=200)
+
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('DRAFT','Draft'),
+            ('DONE','Done')
+        ],
+        default='DRAFT'
+    )
+
+    def __str__(self):
+        return f"Delivery {self.id} - {self.customer}"
+
+class DeliveryItem(models.Model):
+
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE, related_name="items")
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    quantity = models.IntegerField()
+
+class Transfer(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    from_warehouse = models.ForeignKey(
+        Warehouse,
+        related_name="transfer_from",
+        on_delete=models.CASCADE
+    )
+
+    to_warehouse = models.ForeignKey(
+        Warehouse,
+        related_name="transfer_to",
+        on_delete=models.CASCADE
+    )
+
+    quantity = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Adjustment(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+
+    counted_quantity = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
